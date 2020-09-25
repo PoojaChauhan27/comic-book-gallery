e0b8fd2bb0a43cdf0f80eb1c5443fce41b594e8f
Ankush1609 <ac16kevin@gmail.com>
2020-09-25 17:12:43 +0530

    updated comic book detail view using strongly typed view

--- src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -8,7 +8,7 @@ namespace ComicBookGallery.Models
   public class ComicBook
   {
     public int Id { get; set; }
-    public string SeriseTitle { get; set; }
+    public string SeriesTitle { get; set; }
     public int IssueNumber { get; set; }
     public string DescriptionHtml{ get; set; }
     public Artist[] Artists { get; set; }
@@ -18,7 +18,7 @@ namespace ComicBookGallery.Models
     {
       get
       {
-        return SeriseTitle + " #" + IssueNumber;
+        return SeriesTitle + " #" + IssueNumber;
       }
     }
 
@@ -27,7 +27,7 @@ namespace ComicBookGallery.Models
     {
       get
       {
-        return SeriseTitle.Replace("","-")
+        return SeriesTitle.Replace("","-")
           .ToLower() + "-" + IssueNumber + ".jpg";
       }
     }
